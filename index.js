186f193657294fdcbfe42118651c603e84a6f42a
caijiexuan <caijiexuan@ffcs.cn>
2019-04-30 10:13:59 +0800

    增加交互

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ async function setFilter() {
       return;
     } else {
       for (let i = 0; i < Number(ans.selectorNum); i++) {
-        await setFilterContent();
+        await setFilterContent(i);
       }
       filter.length !== 0 && (jsonTemplate.filter = filter);
 
@@ -195,9 +195,10 @@ async function setFilter() {
 }
 /**
  * 设置筛选条件详细内容
+ * @param {Number} num 
  */
-async function setFilterContent() {
-
+async function setFilterContent(num) {
+   utils.successLog(`设置第${num+1}个筛选条件`)
   return inquirer.prompt([
     /**
      * 需要添加几个筛选条件
@@ -248,7 +249,7 @@ async function setTableHeader(isSetting) {
         jsonTemplate.tableHeader || (jsonTemplate.tableHeader = []);
         let result = {};
         for (let i = 0; i < tableHeaderNum; i++) {
-          result = await setHeaderContent();
+          result = await setHeaderContent(i);
           jsonTemplate.tableHeader.push(result);
         }
 
@@ -259,11 +260,12 @@ async function setTableHeader(isSetting) {
 
 }
 /**
- * 
- * 
+ * 设置表头内容
+ * @param {Number} num 
  */
-async function setHeaderContent() {
+async function setHeaderContent(num) {
   let result = {};
+  utils.successLog(`设置第${num+1}表头`)
   await prompt([
     /**
      * 需要添加几个筛选条件
